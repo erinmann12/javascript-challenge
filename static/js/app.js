7b0ce18734c39a2d853e323de0e215792d92767f
Erin Coates Mann <erin.mann2019@gmail.com>
2021-03-22 10:45:56 -0700

    filter by date

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // // Console.log the ufo data from data.js
-console.log(tableData);
+// console.log(tableData);
 
 tableData.forEach(function(ufoReport) {
     console.log(ufoReport);
@@ -19,3 +19,32 @@ tableData.forEach(function(ufoReport) {
       cell.text(value);
     });
 });
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", filterDate);
+
+// Complete the event handler function for the form
+function filterDate() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // console.log(inputElement);
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // console.log(inputValue);
+
+  // Use the form input to filter the data by date
+  var filterData = tableData.filter(ufo => ufo.datetime == inputValue);
+
+  console.log(filterData);
+
+}
